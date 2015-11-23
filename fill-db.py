f12140b5bf51231a4851b4a8d78545427722fa14
mghfdez <mafesan.nsn@gmail.com>
2015-11-23 14:23:16 +0100

    Added namedtuples logic, added function to form insert queries. Pep8 ok

--- fill-db.py
+++ fill-db.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # @ Libresoft, November 2015
+# TODO Make this script for Python3
+
 import csv
 from collections import namedtuple
 import MySQLdb
@@ -12,12 +14,13 @@ DB_PASS = 'operator'
 DB_NAME = 'lkr'
 TABLE = 'lkr_out'
 
+
 def run_query(query=''):
     datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
 
-    conn = MySQLdb.connect(*datos) # Connect to Database
-    cursor = conn.cursor()         # Create cursor
-    cursor.execute(query)          # Run a query
+    conn = MySQLdb.connect(*datos)  # Connect to Database
+    cursor = conn.cursor()          # Create cursor
+    cursor.execute(query)           # Run a query
 
     if query.upper().startswith('SELECT'):
         data = cursor.fetchall()   # Bring SELECT results
@@ -31,17 +34,24 @@ def run_query(query=''):
     return data
 
 
+def form_insert_query(info_list, ntuple):
+
+    my_row = ntuple(*info_lists)
+    query = "INSERT INTO " + TABLE + " VALUES ('"
+    query += my_row.group + "','" + my_row.version
+    query += "'," + str(my_row.size) + "," + str(my_row.sloc) + ");"
+    return query
+
+
 if __name__ == "__main__":
 
     print "Reading <lkr-out.csv> & register data into a DB\r\n"
-    Version = namedtuple('Version', ['group', 'version', 'size', 'sloc'])
+    lversion = namedtuple('Version', ['group', 'version', 'size', 'sloc'])
     lkrReader = csv.reader(open('lkr-out.csv'), delimiter=',', quotechar='|')
+
     for row in lkrReader:
-        group = row[0]
-        version = row[1]
-        size = row[2]
-        sloc = row[3]
-        query = "INSERT INTO " + TABLE + " VALUES ('" + group + "','" + version
-        query += "'," + str(size) + "," + str(sloc) + ");"
+        my_query = form_insert_query(row, lversion)
+        print "Running query: " + query
         run_query(query)
+
     print "...Done\r\n\r\n"
