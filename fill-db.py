d9d93516b76e6c60a0032bf447820db1f27ffa84
Miguel Angel Fernandez <mafesan.nsn@gmail.com>
2015-12-04 11:19:09 +0100

    Fixed key for December in dates dictionary, changed database name

--- fill-db.py
+++ fill-db.py
@@ -11,11 +11,10 @@ import MySQLdb
 DB_HOST = 'localhost'
 DB_USER = 'operator'
 DB_PASS = 'operator'
-DB_NAME = 'linux_kernel_test'
-TABLE = 'version'
+DB_NAME = 'linux_kernel_replication'
 DICC_MONTHS = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04',
                 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08',
-                'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dic': '12', }
+                'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12', }
 
 def run_query(query=''):
     datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
